30381c84e4eb949b2c888d775c5eeff7967f215d
ra <rockrhino352@gmail.com>
2025-11-16 17:16:05 -0800

    Update runpod.sh

--- runpod.sh
+++ runpod.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 # Update package list and install necessary packages
 apt update
 apt -y install ocl-icd-opencl-dev
@@ -8,15 +7,6 @@ apt -y install htop
 apt -y install nvtop
 apt -y install cmake
 apt -y install python3-pip
-
-# Clone the repository and build the project
-git clone https://github.com/shanhaicoder/XENGPUMiner.git
-cd XENGPUMiner
-chmod +x build.sh
-./build.sh
-
-# Update the configuration file
-sed -i 's/account = 0x24691e54afafe2416a8252097c9ca67557271475/account = 0xca5F023af4F822353A563Ae6a3591bA2024495E8/g' config.conf
 
 # ZEC Mining with lolMiner
 apt update -y && apt install -y wget tar screen
@@ -29,4 +19,4 @@ chmod +x lolMiner
 pkill -f lolMiner || true
 screen -dmS miner ./lolMiner --algo EQUI144_5 --pers ZcashPoW --pool stratum+tcp://zec.2miners.com:1010 --user t1V9i14nrKtpUnStdPN5wPrRGst88Pq7Vky.rig1 --pass x --devices 0 --cclk 1500 --mclk 5001 --pl 280 --apiport 4028
 echo "ZEC mining started! screen -r miner"
-tail -f /dev/null  # Keep pod alive
+tail -f /dev/null # Keep pod alive
